346c3d9d68424e8da54c83b22e16e27f18bea045
Mykola Romeiko <mykola@stakelogic.com>
2022-08-09 07:19:05 +0300

    added migrations related to favourite color human model

--- apps/humans/admin.py
+++ apps/humans/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Human
+from .models import Human, Color
 
 admin.site.register(Human)
+admin.site.register(Color)
--- apps/humans/models.py
+++ apps/humans/models.py
@@ -2,10 +2,31 @@ from django.core.validators import MaxValueValidator
 from django.db import models
 
 
+class Colors(models.TextChoices):
+    BLACK = 'black', 'Black'
+    WHITE = 'white', 'White'
+    RED = 'red', 'Red'
+
+
+class Color(models.Model):
+    name = models.CharField(verbose_name='Name', help_text='The name of the color.', max_length=32)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def __repr__(self):
+        return f"<Color - {self.name}>"
+
+
 # Create your models here.
 class Human(models.Model):
     name = models.CharField(verbose_name='Name', help_text='It is a name of a human.', max_length=200)
     age = models.PositiveSmallIntegerField(verbose_name='Age', help_text='How old is this human.', validators=[MaxValueValidator(150)])
+
+    favourite_color = models.CharField(verbose_name='Favourite color', max_length=32, choices=Colors.choices, default=Colors.WHITE)
+
+    favourite_color_by_foreign_key = models.ForeignKey(to=Color, on_delete=models.SET_NULL, null=True, blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
