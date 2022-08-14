e4cd4c40048e4a03d712ab42b410a1e4d01a733f
Mykola Romeiko <mykola@stakelogic.com>
2022-08-14 14:55:22 +0300

    followed django lesson related to admin

--- apps/humans/admin.py
+++ apps/humans/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 
-from .models import Human, Color
+from .models import Human, Color, SuperHuman
 
 admin.site.register(Human)
-admin.site.register(Color)
+
+
+# admin.site.register(Color)
+# admin.site.register(SuperHuman)
+
+class SuperHumanAdminInline(admin.TabularInline):
+    model = SuperHuman
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    inlines = (
+        SuperHumanAdminInline,
+    )
+
+
+@admin.register(SuperHuman)
+class SuperHumanAdmin(admin.ModelAdmin):
+    ...
--- apps/humans/models.py
+++ apps/humans/models.py
@@ -2,7 +2,7 @@ from django.core.validators import MaxValueValidator
 from django.db import models
 
 
-class Colors(models.TextChoices):
+class ColorsChoices(models.TextChoices):
     BLACK = 'black', 'Black'
     WHITE = 'white', 'White'
     RED = 'red', 'Red'
@@ -18,14 +18,27 @@ class Color(models.Model):
         return f"<Color - {self.name}>"
 
 
-# Create your models here.
 class Human(models.Model):
     name = models.CharField(verbose_name='Name', help_text='It is a name of a human.', max_length=200)
     age = models.PositiveSmallIntegerField(verbose_name='Age', help_text='How old is this human.', validators=[MaxValueValidator(150)])
 
-    favourite_color = models.CharField(verbose_name='Favourite color', max_length=32, choices=Colors.choices, default=Colors.WHITE)
+    favourite_color = models.CharField(
+        verbose_name='Favourite color',
+        max_length=32,
+        choices=ColorsChoices.choices,
+        default=ColorsChoices.WHITE)
 
-    favourite_color_by_foreign_key = models.ForeignKey(to=Color, on_delete=models.SET_NULL, null=True, blank=True)
+    favourite_color_by_foreign_key = models.ForeignKey(
+        to=Color,
+        related_name='humans_related_by_foreign_key',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True)
+
+    favourite_color_many_to_many = models.ManyToManyField(
+        to=Color,
+        related_name='human_related_by_many_to_many'
+    )
 
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
@@ -35,3 +48,16 @@ class Human(models.Model):
 
     def __repr__(self):
         return f"<Human (name:{self.name}, age:{self.age})>"
+
+
+class SuperHuman(Human):
+    level = models.PositiveIntegerField(
+        verbose_name='Level',
+        help_text='Level of power.'
+    )
+
+    def __str__(self):
+        return f"{self.name} - {self.age}"
+
+    def __repr__(self):
+        return f"<SuperHuman (name:{self.name}, age:{self.age})>"
